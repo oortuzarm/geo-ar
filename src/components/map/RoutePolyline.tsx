57c60efa49285292ed3ec70141d832dc414768bd
Osvaldo Ortuzar <ohortuzar@gmail.com>
2026-05-08 21:25:08 -0400

    Simplify navigation to compass orientation

--- src/components/map/RoutePolyline.tsx
+++ src/components/map/RoutePolyline.tsx
@@ -2,40 +2,9 @@ import { Polyline } from 'react-leaflet'
 
 interface RoutePolylineProps {
   latLngs: [number, number][]
-  navMode?: boolean
 }
 
-/** Draws the walking route polyline. In navMode renders a glow + main layer. */
-export default function RoutePolyline({ latLngs, navMode }: RoutePolylineProps) {
-  if (navMode) {
-    return (
-      <>
-        {/* Glow / halo layer */}
-        <Polyline
-          positions={latLngs}
-          pathOptions={{
-            color: '#1d4ed8',
-            weight: 18,
-            opacity: 0.22,
-            lineCap: 'round',
-            lineJoin: 'round',
-          }}
-        />
-        {/* Main route line */}
-        <Polyline
-          positions={latLngs}
-          pathOptions={{
-            color: '#3b82f6',
-            weight: 8,
-            opacity: 1,
-            lineCap: 'round',
-            lineJoin: 'round',
-          }}
-        />
-      </>
-    )
-  }
-
+export default function RoutePolyline({ latLngs }: RoutePolylineProps) {
   return (
     <Polyline
       positions={latLngs}
